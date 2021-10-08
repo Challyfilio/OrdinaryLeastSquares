9795898e828d4d9cba1db2d5f2ca11a36028d6e8
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-08 14:53:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 x = np.array([0, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 y = np.array([1.2, 1.5, 1.7, 2, 2.24, 2.4, 2.75, 3])
 ω = np.array([1, 1, 50, 1, 1, 1, 1, 1])
-plt.scatter(x, y, marker='o')
-plt.show()
 
 
-# 生成系数矩阵A
-def gen_coefficient_matrix(X, Y):
-    N = len(X)
+# plt.scatter(x, y, marker='o')
+# plt.show()
+
+
+# 系数矩阵A
+def gen_coefficient_matrix(X, omega):
     m = 3
     A = []
     # 计算每一个方程的系数
@@ -18,31 +19,32 @@ def gen_coefficient_matrix(X, Y):
         a = []
         # 计算当前方程中的每一个系数
         for j in range(m):
-            a.append(sum(X ** (i + j)))
+            a.append(sum(omega * (X ** (i + j))))
         A.append(a)
     return A
 
 
-# #
 # 计算方程组的右端向量b
-def gen_right_vector(X, Y):
-    N = len(X)
+def gen_right_vector(X, Y, omega):
     m = 3
     b = []
     for i in range(m):
-        b.append(sum(X ** i * Y))
+        b.append(sum(omega * (X ** i * Y)))
     return b
 
 
-A = gen_coefficient_matrix(x, y)
-b = gen_right_vector(x, y)
-# #
-a0, a1, a2 = np.linalg.solve(A, b)
-#
-# 生成拟合曲线的绘制点
-_X = np.arange(0, 1, 0.01)
-_Y = np.array([a0 + a1 * x + a2 * x ** 2 for x in _X])
+A = gen_coefficient_matrix(x, ω)
+b = gen_right_vector(x, y, ω)
+print(A)
+print(b)
 
-plt.plot(x, y, 'ro', _X, _Y, 'b', linewidth=2)
+a0, a1, a2 = np.linalg.solve(A, b)
+print(a0, a1, a2)
+
+# 拟合曲线描点
+X = np.arange(0, 1, 0.01)
+Y = np.array([a0 + a1 * x + a2 * x ** 2 for x in X])
+
+plt.plot(x, y, 'ro', X, Y, 'b')
 plt.title("y = {} + {}x + {}$x^2$ ".format(a0, a1, a2))
 plt.show()
