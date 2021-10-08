81e1c3f2e4b8f0b2f2785d4a7ca253122b3a0dfc
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-08 15:04:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ y = np.array([1.2, 1.5, 1.7, 2, 2.24, 2.4, 2.75, 3])
 # plt.scatter(x, y, marker='o')
 # plt.show()
 
-
+# 解方程
 # 系数矩阵A
 def gen_coefficient_matrix(X, omega):
     m = 3
@@ -19,7 +19,7 @@ def gen_coefficient_matrix(X, omega):
         a = []
         # 计算当前方程中的每一个系数
         for j in range(m):
-            a.append(sum(omega * (X ** (i + j))))
+            a.append(sum(omega * (X ** (i + j))))  # ω*x^(i+j)
         A.append(a)
     return A
 
@@ -29,7 +29,7 @@ def gen_right_vector(X, Y, omega):
     m = 3
     b = []
     for i in range(m):
-        b.append(sum(omega * (X ** i * Y)))
+        b.append(sum(omega * (X ** i * Y)))  # ω*x^i*y
     return b
 
 
@@ -39,12 +39,12 @@ print(A)
 print(b)
 
 a0, a1, a2 = np.linalg.solve(A, b)
-print(a0, a1, a2)
+print('a0=', a0, 'a1=', a1, 'a2=', a2)
 
 # 拟合曲线描点
 X = np.arange(0, 1, 0.01)
 Y = np.array([a0 + a1 * x + a2 * x ** 2 for x in X])
 
 plt.plot(x, y, 'ro', X, Y, 'b')
-plt.title("y = {} + {}x + {}$x^2$ ".format(a0, a1, a2))
+plt.title("y = {:.5f} + {:.5f}x + {:.5f}$x^2$ ".format(a0, a1, a2))
 plt.show()
