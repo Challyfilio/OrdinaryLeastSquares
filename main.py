57528d17dab9ece7961a03e61b84adb61b80bae2
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-08 13:54:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x=[0,0.3,0.5,0.6,0.7,0.8,0.9,1.0]
+y=[1.2,1.5,1.7,2,2.24,2.4,2.75,3]
+ω=[1,1,50,1,1,1,1,1]
